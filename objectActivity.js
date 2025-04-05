8c6f9d8052ec446c1db38e35debfb6929467de75
sahagunprincess206 <sahagunprincess206@gmail.com>
2025-04-05 19:14:39 +0800

    Add files via upload

---
+++ objectActivity.js
@@ -0,0 +1,33 @@
+// Define the named car with the  initial properties 
+let car = {
+  type: "SUV's",
+  model: "Corolla Cross",
+  color: "Black"
+};
+
+// Display the type of car in the console
+console.log(typeof car);
+
+// Modify the type of the car to Toyota and use console to update
+car.type = "Toyota";
+console.log(car);
+
+// Add the new property of car  with a value of 4 wheels and use console to update the object
+car.wheels = 4;
+console.log(car);// Define the object named car with the initial properties
+let car = {
+  type: "SUV's",
+  model: "Corolla Cross",
+  color: "Black"
+};
+
+// Display the type of car using the console
+console.log(typeof car);
+
+// Modify the type of the car  to Toyota and log the updated object
+car.type = "Toyota";
+console.log(car);
+
+// Add the new property of car with a value of 4 wheels and use console to updated the object
+car.wheels = 4;
+console.log(car);
